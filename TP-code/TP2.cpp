999ec86e8bad6e66017d1bc87b04ef5a41dd82a3
Ahziel <guillaume.versal@live.com>
2017-04-28 18:03:15 +0200

    SSD

--- TP-code/TP2.cpp
+++ TP-code/TP2.cpp
@@ -6,6 +6,7 @@
 #include <visp/vpImageSimulator.h>
 #include <visp/vpDisplayX.h>
 #include <visp/vpImageConvert.h>
+#include <visp/vpImageFilter.h>
 #include <math.h>
 
 using namespace std ;
@@ -19,10 +20,10 @@ void displayImage(vpImage<unsigned char> img, int posX, int posY, const char *ti
     vpDisplay::close(img);
 }
 
-float convolution(const vpImage< unsigned char > &I, int i, int j, const vpMatrix &K)
+float SSD(const vpImage< unsigned char > &I,const vpImage< unsigned char > &I2, int i, int j, int e, double* &K, const int size)
 {
-    int rows = K.getRows();
-    int cols = K.getCols();
+    int rows = size;
+    int cols = size;
     int heigth = I.getHeight();
     int width = I.getCols();
 
@@ -42,14 +43,67 @@ float convolution(const vpImage< unsigned char > &I, int i, int j, const vpMatri
                 sum+=0;
 
             else
-                sum += I[i+k][j+l]*K[m][n];
+                sum += K[m*size+n] * pow((I2[i+k][e+l] - I[i+k][j+l]),2);
             n++;
         }
         m++;
     }
 
+//    std::cout << sum << std::endl;
+
     return sum;
 
+}
+
+void computeDisparityWTA(vpImage<unsigned char> & img_1, vpImage<unsigned char> & img_2,vpImage<unsigned char> & disparity_map)
+{
+
+  for(int i = 0; i < img_1.getHeight(); i++) {
+    for(int j = 0; j < img_1.getWidth(); j++) {
+
+      float crit = abs(img_2[i][0] - img_1[i][j]);
+      float disparity = 0;
+
+      for(int k = 1; k < img_2.getWidth(); k++) {
+
+        float new_crit = abs(img_2[i][k] - img_1[i][j]);
+
+        if(new_crit < crit) {
+          crit = new_crit;
+          disparity = k;
+
+        }
+      }
+
+      disparity_map[i][j] = disparity;
+
+    }
+  }
+}
+
+void computeDisparityAC(vpImage<unsigned char> & img_1, vpImage<unsigned char> & img_2, double* K, int size, vpImage<float> & disparity_map)
+{
+
+  for(int i = 0; i < img_1.getHeight(); i++) {
+    for(int j = 0; j < img_1.getWidth(); j++) {
+
+      float crit =  SSD(img_1,img_2,i,j,0,K, size);;
+      float disparity = 0;
+
+      for(int k = 1; k < img_2.getWidth(); k++) {
+
+        float new_crit = SSD(img_1,img_2,i,j,k,K, size);
+
+        if(new_crit < crit) {
+          crit = new_crit;
+          disparity = k;
+        }
+      }
+
+      disparity_map[i][j] = disparity;
+
+    }
+  }
 }
 
 int main()
@@ -63,26 +117,13 @@ int main()
   vpImage<float> disparity_map;
   disparity_map.resize(img_1.getHeight(), img_1.getWidth());
 
-  for(int i = 0; i < img_1.getHeight(); i++) {
-    for(int j = 0; j < img_1.getWidth(); j++) {
-      float crit = abs(img_2[i][0] - img_1[i][j]);
-      float disparity = 0;
+  //computeDisparityWTA(img_1,img_2,disparity_map);
+  int size = 3;
+  double kernel[size];
+  vpImageFilter::getGaussianKernel(kernel, size);
 
-      for(int k = 1; k < img_2.getWidth(); k++) {
-        float new_crit = abs(img_2[i][k] - img_1[i][j]);
 
-        if(new_crit < crit) {
-          crit = new_crit;
-          disparity = k;
-
-        }
-
-      }
-
-      disparity_map[i][j] = disparity;
-
-    }
-  }
+  computeDisparityAC(img_1,img_2, kernel, size,disparity_map);
 
   vpImage<unsigned char> disparity_map_uchar;
   vpImageConvert::convert(disparity_map, disparity_map_uchar);
