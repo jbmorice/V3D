63218c44587a01f2e818fb8b4de993dc69ae7662
Jean-Baptiste Morice <mail@jbmorice.com>
2017-04-28 16:57:37 +0200

    Winner Take All done

--- TP-code/TP2.cpp
+++ TP-code/TP2.cpp
@@ -19,37 +19,30 @@ void displayImage(vpImage<unsigned char> img, int posX, int posY, const char *ti
     vpDisplay::close(img);
 }
 
-float convolution(const vpImage< unsigned char > &I, int i, int j, const vpMatrix &K)
+void winnerTakesAll(const vpImage<unsigned char> & img_1, const vpImage<unsigned char> & img_2, vpImage<float> & disparity_map)
 {
-    int rows = K.getRows();
-    int cols = K.getCols();
-    int heigth = I.getHeight();
-    int width = I.getCols();
+    disparity_map.resize(img_1.getHeight(), img_1.getWidth());
 
-    double sum = 0;
-    int m = 0;
-    int n = 0;
+    for(int i = 0; i < img_1.getHeight(); i++) {
+      for(int j = 0; j < img_1.getWidth(); j++) {
+        float crit = abs(img_2[i][0] - img_1[i][j]);
+        float disparity = 0;
 
-    for(int k = -rows/2; k<=rows/2; k++)
-    {
-        n=0;
-        for(int l = -cols/2; l<=cols/2; l++)
-        {
-            if(i+k<0 || i+k>=heigth)
-                sum+=0;
+        for(int k = 1; k < img_2.getWidth(); k++) {
+          float new_crit = abs(img_2[i][k] - img_1[i][j]);
 
-            else if(j+l<0 || j+l>=width)
-                sum+=0;
+          if(new_crit < crit) {
+            crit = new_crit;
+            disparity = k;
 
-            else
-                sum += I[i+k][j+l]*K[m][n];
-            n++;
+          }
+
         }
-        m++;
+
+        disparity_map[i][j] = disparity;
+
+      }
     }
-
-    return sum;
-
 }
 
 int main()
@@ -61,28 +54,7 @@ int main()
   vpImageIo::read(img_2, "../data/tsukuba-r.jpg");
 
   vpImage<float> disparity_map;
-  disparity_map.resize(img_1.getHeight(), img_1.getWidth());
-
-  for(int i = 0; i < img_1.getHeight(); i++) {
-    for(int j = 0; j < img_1.getWidth(); j++) {
-      float crit = abs(img_2[i][0] - img_1[i][j]);
-      float disparity = 0;
-
-      for(int k = 1; k < img_2.getWidth(); k++) {
-        float new_crit = abs(img_2[i][k] - img_1[i][j]);
-
-        if(new_crit < crit) {
-          crit = new_crit;
-          disparity = k;
-
-        }
-
-      }
-
-      disparity_map[i][j] = disparity;
-
-    }
-  }
+  winnerTakesAll(img_1, img_2, disparity_map);
 
   vpImage<unsigned char> disparity_map_uchar;
   vpImageConvert::convert(disparity_map, disparity_map_uchar);
